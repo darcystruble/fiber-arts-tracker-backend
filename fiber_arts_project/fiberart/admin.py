95095b7d00e3bd840a91363f3df00b767d55e7e9
darcystruble <darcyhallstruble@gmail.com>
2023-11-28 17:04:56 -0500

    delete and start over

--- fiber_arts_project/fiberart/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Project, Stash, Goal
-
-# Register your models here.
-admin.site.register(Project)
-admin.site.register(Stash)
-admin.site.register(Goal)
--- fiber_arts_project/settings.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE fiberart;
-CREATE USER fiberartuser WITH PASSWORD 'fiberart';
-GRANT ALL PRIVILEGES ON DATABASE fiberart TO fiberartuser;
